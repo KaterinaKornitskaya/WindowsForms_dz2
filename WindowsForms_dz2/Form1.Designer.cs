9c0a57b6574a791e58dbc382115a5e9df833f73b
Радченко Катерина <ketrinradchenko@gmail.com>
2023-05-05 17:56:34 +0300

    Add project files.

---
+++ WindowsForms_dz2/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace WindowsForms_dz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_f_name = new System.Windows.Forms.Label();
+            this.textBox_f_name = new System.Windows.Forms.TextBox();
+            this.label_age = new System.Windows.Forms.Label();
+            this.textBox_age = new System.Windows.Forms.TextBox();
+            this.textBox_l_name = new System.Windows.Forms.TextBox();
+            this.label_l_name = new System.Windows.Forms.Label();
+            this.label_gender = new System.Windows.Forms.Label();
+            this.label_date_of_birth = new System.Windows.Forms.Label();
+            this.label_marital_status = new System.Windows.Forms.Label();
+            this.textBox_marital_status = new System.Windows.Forms.TextBox();
+            this.textBox_date_of_birth = new System.Windows.Forms.TextBox();
+            this.textBox_gender = new System.Windows.Forms.TextBox();
+            this.label_add_info = new System.Windows.Forms.Label();
+            this.textBox_add_info = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_load = new System.Windows.Forms.Button();
+            this.label_manual = new System.Windows.Forms.Label();
+            this.label_thank_you = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Controls.Add(this.label_thank_you);
+            this.groupBox1.Controls.Add(this.label_manual);
+            this.groupBox1.Controls.Add(this.button_load);
+            this.groupBox1.Controls.Add(this.button_save);
+            this.groupBox1.Controls.Add(this.textBox_add_info);
+            this.groupBox1.Controls.Add(this.label_add_info);
+            this.groupBox1.Controls.Add(this.textBox_gender);
+            this.groupBox1.Controls.Add(this.textBox_date_of_birth);
+            this.groupBox1.Controls.Add(this.textBox_marital_status);
+            this.groupBox1.Controls.Add(this.label_marital_status);
+            this.groupBox1.Controls.Add(this.label_date_of_birth);
+            this.groupBox1.Controls.Add(this.label_gender);
+            this.groupBox1.Controls.Add(this.label_l_name);
+            this.groupBox1.Controls.Add(this.textBox_l_name);
+            this.groupBox1.Controls.Add(this.textBox_age);
+            this.groupBox1.Controls.Add(this.label_age);
+            this.groupBox1.Controls.Add(this.textBox_f_name);
+            this.groupBox1.Controls.Add(this.label_f_name);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 657);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Person Information";
+            // 
+            // label_f_name
+            // 
+            this.label_f_name.AutoSize = true;
+            this.label_f_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_f_name.Location = new System.Drawing.Point(9, 130);
+            this.label_f_name.Name = "label_f_name";
+            this.label_f_name.Size = new System.Drawing.Size(86, 20);
+            this.label_f_name.TabIndex = 0;
+            this.label_f_name.Text = "First Name";
+            // 
+            // textBox_f_name
+            // 
+            this.textBox_f_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_f_name.Location = new System.Drawing.Point(175, 130);
+            this.textBox_f_name.Name = "textBox_f_name";
+            this.textBox_f_name.Size = new System.Drawing.Size(248, 29);
+            this.textBox_f_name.TabIndex = 1;
+            // 
+            // label_age
+            // 
+            this.label_age.AutoSize = true;
+            this.label_age.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_age.Location = new System.Drawing.Point(9, 200);
+            this.label_age.Name = "label_age";
+            this.label_age.Size = new System.Drawing.Size(38, 20);
+            this.label_age.TabIndex = 2;
+            this.label_age.Text = "Age";
+            // 
+            // textBox_age
+            // 
+            this.textBox_age.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_age.Location = new System.Drawing.Point(175, 200);
+            this.textBox_age.Name = "textBox_age";
+            this.textBox_age.Size = new System.Drawing.Size(248, 29);
+            this.textBox_age.TabIndex = 3;
+            // 
+            // textBox_l_name
+            // 
+            this.textBox_l_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_l_name.Location = new System.Drawing.Point(175, 165);
+            this.textBox_l_name.Name = "textBox_l_name";
+            this.textBox_l_name.Size = new System.Drawing.Size(248, 29);
+            this.textBox_l_name.TabIndex = 4;
+            // 
+            // label_l_name
+            // 
+            this.label_l_name.AutoSize = true;
+            this.label_l_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_l_name.Location = new System.Drawing.Point(9, 165);
+            this.label_l_name.Name = "label_l_name";
+            this.label_l_name.Size = new System.Drawing.Size(86, 20);
+            this.label_l_name.TabIndex = 5;
+            this.label_l_name.Text = "Last Name";
+            // 
+            // label_gender
+            // 
+            this.label_gender.AutoSize = true;
+            this.label_gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_gender.Location = new System.Drawing.Point(9, 236);
+            this.label_gender.Name = "label_gender";
+            this.label_gender.Size = new System.Drawing.Size(63, 20);
+            this.label_gender.TabIndex = 6;
+            this.label_gender.Text = "Gender";
+            // 
+            // label_date_of_birth
+            // 
+            this.label_date_of_birth.AutoSize = true;
+            this.label_date_of_birth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_date_of_birth.Location = new System.Drawing.Point(9, 271);
+            this.label_date_of_birth.Name = "label_date_of_birth";
+            this.label_date_of_birth.Size = new System.Drawing.Size(99, 20);
+            this.label_date_of_birth.TabIndex = 7;
+            this.label_date_of_birth.Text = "Date of Birth";
+            // 
+            // label_marital_status
+            // 
+            this.label_marital_status.AutoSize = true;
+            this.label_marital_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_marital_status.Location = new System.Drawing.Point(9, 306);
+            this.label_marital_status.Name = "label_marital_status";
+            this.label_marital_status.Size = new System.Drawing.Size(107, 20);
+            this.label_marital_status.TabIndex = 8;
+            this.label_marital_status.Text = "Marital Status";
+            // 
+            // textBox_marital_status
+            // 
+            this.textBox_marital_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_marital_status.Location = new System.Drawing.Point(175, 306);
+            this.textBox_marital_status.Name = "textBox_marital_status";
+            this.textBox_marital_status.Size = new System.Drawing.Size(248, 29);
+            this.textBox_marital_status.TabIndex = 9;
+            // 
+            // textBox_date_of_birth
+            // 
+            this.textBox_date_of_birth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_date_of_birth.Location = new System.Drawing.Point(175, 271);
+            this.textBox_date_of_birth.Name = "textBox_date_of_birth";
+            this.textBox_date_of_birth.Size = new System.Drawing.Size(248, 29);
+            this.textBox_date_of_birth.TabIndex = 10;
+            // 
+            // textBox_gender
+            // 
+            this.textBox_gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_gender.Location = new System.Drawing.Point(175, 236);
+            this.textBox_gender.Name = "textBox_gender";
+            this.textBox_gender.Size = new System.Drawing.Size(248, 29);
+            this.textBox_gender.TabIndex = 11;
+            // 
+            // label_add_info
+            // 
+            this.label_add_info.AutoSize = true;
+            this.label_add_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_add_info.Location = new System.Drawing.Point(9, 341);
+            this.label_add_info.Name = "label_add_info";
+            this.label_add_info.Size = new System.Drawing.Size(160, 20);
+            this.label_add_info.TabIndex = 12;
+            this.label_add_info.Text = "Additional Infornation";
+            // 
+            // textBox_add_info
+            // 
+            this.textBox_add_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_add_info.Location = new System.Drawing.Point(175, 341);
+            this.textBox_add_info.Name = "textBox_add_info";
+            this.textBox_add_info.Size = new System.Drawing.Size(248, 29);
+            this.textBox_add_info.TabIndex = 13;
+            // 
+            // button_save
+            // 
+            this.button_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_save.Location = new System.Drawing.Point(13, 408);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(195, 58);
+            this.button_save.TabIndex = 14;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // button_load
+            // 
+            this.button_load.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_load.Location = new System.Drawing.Point(227, 408);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(196, 58);
+            this.button_load.TabIndex = 15;
+            this.button_load.Text = "Load";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // label_manual
+            // 
+            this.label_manual.AutoSize = true;
+            this.label_manual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_manual.Location = new System.Drawing.Point(9, 29);
+            this.label_manual.Name = "label_manual";
+            this.label_manual.Size = new System.Drawing.Size(61, 20);
+            this.label_manual.TabIndex = 16;
+            this.label_manual.Text = "Manual";
+            // 
+            // label_thank_you
+            // 
+            this.label_thank_you.AutoSize = true;
+            this.label_thank_you.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_thank_you.Location = new System.Drawing.Point(129, 541);
+            this.label_thank_you.Name = "label_thank_you";
+            this.label_thank_you.Size = new System.Drawing.Size(183, 37);
+            this.label_thank_you.TabIndex = 17;
+            this.label_thank_you.Text = "Thank You!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 681);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_f_name;
+        private System.Windows.Forms.Label label_f_name;
+        private System.Windows.Forms.Label label_l_name;
+        private System.Windows.Forms.TextBox textBox_l_name;
+        private System.Windows.Forms.TextBox textBox_age;
+        private System.Windows.Forms.Label label_age;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.TextBox textBox_add_info;
+        private System.Windows.Forms.Label label_add_info;
+        private System.Windows.Forms.TextBox textBox_gender;
+        private System.Windows.Forms.TextBox textBox_date_of_birth;
+        private System.Windows.Forms.TextBox textBox_marital_status;
+        private System.Windows.Forms.Label label_marital_status;
+        private System.Windows.Forms.Label label_date_of_birth;
+        private System.Windows.Forms.Label label_gender;
+        private System.Windows.Forms.Label label_manual;
+        private System.Windows.Forms.Label label_thank_you;
+    }
+}
+
---
+++ WindowsForms_dz2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsForms_dz2
+{
+    //     Задание 1
+    // Создайте приложение размером до 720х480 пикселов и разместите
+    // на главной форме необходимые элементы управления для получения информации:
+    // ■ Фамилия
+    // ■ Имя
+    // ■ Отчество
+    // ■ Пол
+    // ■ Год и дата рождения
+    // ■ Семейный статус
+    // ■ Дополнительная информация
+    // Добавьте кнопку Save и обработчик нажатия кнопки для сохранения информации
+    // из элементов управления в файл.
+    // Добавьте кнопку Load и обработчик нажатия кнопки для восстановления
+    // информации из файла в элементы управления.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 480;  // ширина рабочей области
+            this.Height = 720;  // высота рабочей области
+            // текс для кнопки "Инструкция"
+            label_manual.Text = "Заполните поля формы. \nНажмите 'Save', чтобы сохранить данные в файл" +
+                "\nили 'Load', чтобы загрузить данные из файла.";          
+        }
+
+        private void button_save_Click(object sender, EventArgs e)  // обработчик клика на кнопку Save
+        {
+            string my_file = "Form.txt";
+            using (FileStream fs = new FileStream(my_file, FileMode.Create,
+                FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode))
+                {
+                    sw.WriteLine(textBox_f_name.Text);
+                    sw.WriteLine(textBox_l_name.Text);
+                    sw.WriteLine(textBox_age.Text);
+                    sw.WriteLine(textBox_gender.Text);
+                    sw.WriteLine(textBox_date_of_birth.Text);
+                    sw.WriteLine(textBox_marital_status.Text);
+                    sw.WriteLine(textBox_add_info.Text);
+                }
+            }
+        }
+        private void button_load_Click(object sender, EventArgs e)  // обработчик клика на кнопку Load
+        {
+            string my_file = "Form.txt";
+            using (FileStream fs = new FileStream(my_file, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.Unicode))
+                {
+                    textBox_f_name.Text = sr.ReadLine();
+                    textBox_l_name.Text = sr.ReadLine();
+                    textBox_age.Text = sr.ReadLine();
+                    textBox_gender.Text = sr.ReadLine();
+                    textBox_date_of_birth.Text = sr.ReadLine();
+                    textBox_marital_status.Text = sr.ReadLine();
+                    textBox_add_info.Text = sr.ReadLine();
+                }
+            }
+        }
+    }
+}
